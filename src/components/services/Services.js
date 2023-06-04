bf256cec6a56f270d1660dd7472e623ee3684715
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-06-03 18:08:20 -0700

    responsive fix

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -8,6 +8,7 @@ const Services = () => {
   const { ref, inView } = useInView({
     triggeOnce: false,
   });
+
   return (
     <section
       id="services"
@@ -81,41 +82,65 @@ const Services = () => {
           </div>
         )}
       </div>
-
-      <div className="card">
-        <div className="card-Right">
-          <div className="card-Title text-responsive">
-            <h1 className="text-4xl md:text-5xl  text-designColor text-center font-bold capitalize">
-              Fountain Cleaning and Repair
-            </h1>{" "}
+      <div ref={ref}>
+        {inView ? (
+          <div className=" revealRight card ">
+            <div className="card-picture">
+              <img
+                src={IvanRepair}
+                alt={IvanRepair}
+                style={{ borderRadius: "10px" }}
+              />
+            </div>
+            <div className="card-Right">
+              <div className="card-Title text-responsive">
+                <h1 className="text-4xl md:text-5xl  text-designColor text-center font-bold capitalize">
+                  Fountain Cleaning and Repair
+                </h1>
+              </div>
+              <div className="card-paragraph text-responsive">
+                <p1 className="text-xs mdl:text-sm uppercase font-light  text-container text-designColor text-center tracking-wide">
+                  Is there a fountain you would like to install in your home or
+                  business, but it looks too heavy or complicated? Let the
+                  professionals take care of it for you. We pick up deliver and
+                  install all day every day. With us you will ensure your
+                  investment will be handled and installed properly. It does not
+                  matter where we pick it up at a store or even another house.
+                </p1>
+              </div>
+            </div>
           </div>
-          <div className="card-paragraph text-responsive">
-            <p1 className="text-xs mdl:text-sm  uppercase font-light text-container text-designColor text-center tracking-wide">
-              Is there a fountain you would like to install in your home or
-              business, but it looks too heavy or complicated? Let the
-              professionals take care of it for you. We pick up deliver and
-              install all day every day. With us you will ensure your investment
-              will be handled and installed properly. It does not matter where
-              we pick it up at a store or even another house.
-            </p1>
+        ) : (
+          <div className=" hideRight card ">
+            <div className="card-picture">
+              <img
+                src={IvanRepair}
+                alt={IvanRepair}
+                style={{ borderRadius: "10px" }}
+              />
+            </div>
+            <div className="card-Right">
+              <div className="card-Title text-responsive">
+                <h1 className="text-4xl md:text-5xl  text-designColor text-center font-bold capitalize">
+                  Fountain Cleaning and Repair
+                </h1>
+              </div>
+              <div className="card-paragraph text-responsive">
+                <p1 className="text-xs mdl:text-sm uppercase font-light  text-container text-designColor text-center tracking-wide">
+                  Is there a fountain you would like to install in your home or
+                  business, but it looks too heavy or complicated? Let the
+                  professionals take care of it for you. We pick up deliver and
+                  install all day every day. With us you will ensure your
+                  investment will be handled and installed properly. It does not
+                  matter where we pick it up at a store or even another house.
+                </p1>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="card-picture">
-          <img
-            src={IvanRepair}
-            alt={IvanRepair}
-            style={{
-              borderRadius: "10px",
-              position: "flex-start",
-              top: "0px",
-              left: "0px",
-              width: "100%",
-              height: "100%",
-            }}
-          />
-        </div>
+        )}
       </div>
-      <div className="card ">
+
+      <div className=" revealLeft card ">
         <div className="card-picture">
           <img
             src={IvanDelivery}
@@ -125,12 +150,12 @@ const Services = () => {
         </div>
         <div className="card-Right">
           <div className="card-Title text-responsive">
-            <h1 className="text-4xl md:text-5xl text-designColor text-center font-bold capitalize">
+            <h1 className="text-4xl md:text-5xl  text-designColor text-center font-bold capitalize">
               Maintenance
             </h1>
           </div>
           <div className="card-paragraph text-responsive">
-            <p1 className="text-xs mdl:text-sm  uppercase font-light  text-container text-designColor text-center tracking-wide">
+            <p1 className="text-xs mdl:text-sm uppercase font-light  text-container text-designColor text-center tracking-wide">
               Is there a fountain you would like to install in your home or
               business, but it looks too heavy or complicated? Let the
               professionals take care of it for you. We pick up deliver and
@@ -141,37 +166,31 @@ const Services = () => {
           </div>
         </div>
       </div>
-      <div className="card ">
-        <div className="card-Right">
-          <div className="card-Title text-responsive">
-            <h1 className="text-4xl md:text-5xl text-designColor text-center font-bold capitalize">
-              And Much More!
-            </h1>{" "}
-          </div>
-          <div className="card-paragraph text-responsive">
-            <p1 className="text-xs mdl:text-sm  uppercase font-light text-container text-designColor text-center tracking-wide">
-              There is nothing more relaxing then to listen to the tranquil
-              sounds of your fountain. Enjoy your fountain for years to come.
-              Let us maintain your fountain with regular service. Here is the
-              service we can provide for you. Vacuuming dirty water out,
-              scrubbing out algae, cleaning pump filter, checking all tubing and
-              fittings, re-leveling water flow and adding chemicals to water
-            </p1>
-          </div>
-        </div>
+
+      <div className=" card ">
         <div className="card-picture">
           <img
             src={IvanRepair}
             alt={IvanRepair}
-            style={{
-              borderRadius: "10px",
-              position: "flex-start",
-              top: "0px",
-              left: "0px",
-              width: "100%",
-              height: "100%",
-            }}
+            style={{ borderRadius: "10px" }}
           />
+        </div>
+        <div className="card-Right">
+          <div className="card-Title text-responsive">
+            <h1 className="text-4xl md:text-5xl  text-designColor text-center font-bold capitalize">
+              And Much More!
+            </h1>
+          </div>
+          <div className="card-paragraph text-responsive">
+            <p1 className="text-xs mdl:text-sm uppercase font-light  text-container text-designColor text-center tracking-wide">
+              Is there a fountain you would like to install in your home or
+              business, but it looks too heavy or complicated? Let the
+              professionals take care of it for you. We pick up deliver and
+              install all day every day. With us you will ensure your investment
+              will be handled and installed properly. It does not matter where
+              we pick it up at a store or even another house.
+            </p1>
+          </div>
         </div>
       </div>
     </section>
